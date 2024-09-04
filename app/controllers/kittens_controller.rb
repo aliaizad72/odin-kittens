1740d54800445d79546e4b12ba4c44bd4c680c36
Ali Aizad <aliaizad72@gmail.com>
2024-09-04 15:42:59 +0800

    New and create action complete

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -5,4 +5,23 @@ class KittensController < ApplicationController
   def show
     @kitten = Kitten.find_by(id: params[:id])
   end
+
+  def new
+    @kitten = Kitten.new
+  end
+  def create
+    @kitten = Kitten.new(kittens_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def kittens_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,6 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, presence: true
-  validates :age, presence: true
-  validates :cuteness, presence: true
-  validates :softness, presence: true
+  validates :age, numericality: true
+  validates :cuteness, numericality: { less_than_or_equal_to: 10 }
+  validates :softness, numericality: { less_than_or_equal_to: 10 }
 end
