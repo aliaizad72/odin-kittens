a1d00138404bfc47379a23bfff205a2c13d75edd
Ali Aizad <aliaizad72@gmail.com>
2024-09-04 17:24:49 +0800

    Finish the HTML implementation

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -10,18 +10,42 @@ class KittensController < ApplicationController
     @kitten = Kitten.new
   end
   def create
-    @kitten = Kitten.new(kittens_params)
+    @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash[:notice] = "You have created #{@kitten.name}"
       redirect_to @kitten
     else
       render :new, status: 422
     end
   end
 
+  def edit
+    @kitten = Kitten.find_by(id: params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find_by(id: params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:notice] = "You have edited #{@kitten.name}"
+      redirect_to @kitten
+    else
+      render :edit, status: 422
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find_by(id: params[:id])
+    flash[:notice] = "You have deleted #{@kitten.name}"
+    @kitten.destroy
+
+    redirect_to root_path, status: 303
+  end
+
   private
 
-  def kittens_params
+  def kitten_params
     params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
